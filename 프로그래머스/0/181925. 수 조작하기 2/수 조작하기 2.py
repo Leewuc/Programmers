a2cb38ff4e178b1f5c9dd00892885e711e050510
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-08-28 11:27:04 +0900

    [level 0] Title: 수 조작하기 2, Time: 45.82 ms, Memory: 12.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,13 @@
+def solution(numLog):
+    answer = ''
+    for i in range(len(numLog)):
+        if i >= 1:
+            if numLog[i] - numLog[i-1] == 1:
+                answer += 'w'
+            elif numLog[i] - numLog[i-1] == -1:
+                answer += 's'
+            elif numLog[i] - numLog[i-1] == 10:
+                answer += 'd'
+            elif numLog[i] - numLog[i-1] == -10:
+                answer += 'a'
+    return answer
