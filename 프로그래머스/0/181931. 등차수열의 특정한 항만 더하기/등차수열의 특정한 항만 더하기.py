1dc60bef846d0f990f747c0ac2db1ce82785a6ac
Leewoochang <92780253+Leewuc@users.noreply.github.com>
2024-08-28 10:32:39 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.py
@@ -0,0 +1,11 @@
+def solution(a, d, included):
+    answer = 0
+    for i in range(len(included)):
+        if i == 0:
+            if included[i] == 1:
+                answer += a
+        elif i >= 1:
+            if included[i] == 1:
+                answer += a + d * i
+                
+    return answer
